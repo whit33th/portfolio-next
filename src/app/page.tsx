9ecf7f33c072ecd10962103f38631cb37cf7eeb1
Daniil Kaminskyi <deniil019@gmail.com>
2025-03-28 08:03:43 +0100

    Update resume content and formatting for clarity and professionalism

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,10 +29,18 @@ export default function Home() {
           <div className="flex-1 space-y-8">
             <div className="space-y-4">
               <h2 className="text-6xl pb-1 font-bold bg-clip-text text-transparent bg-gradient-to-r from-neutral-200 to-neutral-500">
-                <DecryptedText speed={70} animateOn={"view"} text="Daniil Kaminskyi" />
+                <DecryptedText
+                  speed={70}
+                  animateOn={"view"}
+                  text="Daniil Kaminskyi"
+                />
               </h2>
               <p className="text-lg text-neutral-400">
-                <DecryptedText speed={80} animateOn={"view"} text="Frontend Developer" />
+                <DecryptedText
+                  speed={80}
+                  animateOn={"view"}
+                  text="Frontend Developer"
+                />
               </p>
             </div>
 
@@ -45,7 +53,7 @@ export default function Home() {
                   <DecryptedText
                     speed={50}
                     animateOn={"view"}
-                    text="I am a student passionate about creating websites. During my education and internship, I gained skills in designing and developing websites, including knowledge of React, Next, Tailwind CSS/SCSS/CSS. In my free time, I work on projects that allow me to develop my skills and experiment with new technologies. Exploring new technologies and best practices."
+                    text="I am a professional web developer with expertise in building websites. My experience includes proficiency in React, Next, and Tailwind CSS/SCSS/CSS. I continually engage in projects to refine my skills and explore emerging technologies."
                   />
                 </p>
               </div>
@@ -53,7 +61,11 @@ export default function Home() {
               {/* Experience Section */}
               <div className="space-y-4">
                 <h3 className="text-xl font-semibold text-neutral-200">
-                  <DecryptedText speed={90} animateOn={"view"} text="Experience" />
+                  <DecryptedText
+                    speed={90}
+                    animateOn={"view"}
+                    text="Experience"
+                  />
                 </h3>
                 <div className="space-y-3">
                   {experience.map((exp) => (
@@ -69,10 +81,18 @@ export default function Home() {
                         />
                       </p>
                       <p className="text-sm text-neutral-400">
-                        <DecryptedText speed={60} animateOn={"view"} text={exp.company} />
+                        <DecryptedText
+                          speed={60}
+                          animateOn={"view"}
+                          text={exp.company}
+                        />
                       </p>
                       <p className="text-sm text-neutral-400">
-                        <DecryptedText speed={60} animateOn={"view"} text={exp.period} />
+                        <DecryptedText
+                          speed={60}
+                          animateOn={"view"}
+                          text={exp.period}
+                        />
                       </p>
                       <ul className="mt-2 text-sm text-neutral-400 list-disc ml-4">
                         {exp.responsibilities.map((responsibility, index) => (
@@ -119,7 +139,11 @@ export default function Home() {
               </div>
               <div className="bg-[#0c0c0c] p-4 rounded-xl border border-[#191919]">
                 <p className="font-medium">
-                  <DecryptedText speed={70} animateOn={"view"} text="UI/UX Design" />
+                  <DecryptedText
+                    speed={70}
+                    animateOn={"view"}
+                    text="UI/UX Design"
+                  />
                 </p>
                 <p className="text-sm text-neutral-400">
                   <DecryptedText
@@ -131,7 +155,11 @@ export default function Home() {
               </div>
               <div className="bg-[#0c0c0c] p-4 rounded-xl border border-[#191919]">
                 <p className="font-medium">
-                  <DecryptedText speed={70} animateOn={"view"} text="Web Performance" />
+                  <DecryptedText
+                    speed={70}
+                    animateOn={"view"}
+                    text="Web Performance"
+                  />
                 </p>
                 <p className="text-sm text-neutral-400">
                   <DecryptedText
@@ -158,10 +186,18 @@ export default function Home() {
                 />
               </p>
               <p className="text-sm text-neutral-400 mt-1">
-                <DecryptedText speed={60} animateOn={"view"} text="Poznan, Poland" />
+                <DecryptedText
+                  speed={60}
+                  animateOn={"view"}
+                  text="Poznan, Poland"
+                />
               </p>
               <p className="text-sm text-neutral-400">
-                <DecryptedText speed={60} animateOn={"view"} text="2021 - Present" />
+                <DecryptedText
+                  speed={60}
+                  animateOn={"view"}
+                  text="2021 - 2025"
+                />
               </p>
             </div>
           </div>
@@ -170,7 +206,11 @@ export default function Home() {
         {/* Skills Section */}
         <div className="pt-16 space-y-8">
           <h3 className="text-2xl font-semibold text-neutral-200">
-            <DecryptedText speed={90} animateOn={"view"} text="Skills & Tools" />
+            <DecryptedText
+              speed={90}
+              animateOn={"view"}
+              text="Skills & Tools"
+            />
           </h3>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
             {stack.map((category) => (
@@ -179,11 +219,18 @@ export default function Home() {
                 className="bg-[#0c0c0c] p-6 rounded-xl border border-[#191919]"
               >
                 <h4 className="font-medium mb-4">
-                  <DecryptedText speed={70} animateOn={"view"} text={category.name} />
+                  <DecryptedText
+                    speed={70}
+                    animateOn={"view"}
+                    text={category.name}
+                  />
                 </h4>
                 <div className="grid grid-cols-4 gap-3">
                   {category.content.map((tech) => (
-                    <div key={tech.name} className="flex flex-col items-center gap-1">
+                    <div
+                      key={tech.name}
+                      className="flex flex-col items-center gap-1"
+                    >
                       <Image
                         src={tech.icon}
                         alt={tech.name}
@@ -192,7 +239,11 @@ export default function Home() {
                         className="w-6 h-6"
                       />
                       <span className="text-[10px] text-neutral-400 text-center">
-                        <DecryptedText speed={40} animateOn={"view"} text={tech.name} />
+                        <DecryptedText
+                          speed={40}
+                          animateOn={"view"}
+                          text={tech.name}
+                        />
                       </span>
                     </div>
                   ))}
